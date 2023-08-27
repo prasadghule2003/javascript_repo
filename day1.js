788d4e008235108e82d9d2085f322a2a0b13f3ff
prasad <prasadghule31@gmail.com>
2023-08-27 20:23:12 +0530

    first commit

---
+++ day1.js
@@ -0,0 +1,62 @@
+let t=10
+console.log(t)
+
+const y =5
+console.log(y)
+
+//y = 800
+console.log(y)
+
+//program2
+//arthematic operation
+let j = 8
+let k = 4
+console.log(j+k)
+console.log(j-k)
+console.log(j*k)
+console.log(j/k)
+console.log(j%k)
+
+//program  3
+function sub (a,b){
+    console.log(a+b)
+    console.log(a-b)
+    console.log(a*b)
+    console.log(a/b)
+    console.log(a%b)
+}
+
+sub (50,25)
+sub (6,3)
+
+//program 4
+//function without parameter & without return type
+//function addf(){
+    console.log(7+7)
+//}
+//addf()
+//addf()
+
+//program 5
+//function with parameter & without return type
+
+function addQ(v,m){
+    console.log(v*m)
+}
+addQ(12,3)
+
+//program 6
+//function with parameter & with return type
+function addU(e,r){
+    return e+r
+}
+let q1 =addU(12,8)
+console.log(q1)
+console.log(q1+6)
+console.log(q1/4)
+
+
+
+
+
+
