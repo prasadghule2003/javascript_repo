e5e783bee1220ef5e5543a4d8cb026ba4ddab8ab
prasad <prasadghule31@gmail.com>
2023-09-06 12:45:36 +0530

    TEST

---
+++ DOM/DOM3/day.js
@@ -0,0 +1,13 @@
+// <h4>id = "one" class = "two" name = "ps">prasad4</h4> 
+
+// id
+let idOne = document.querySelector('#one')
+console.log(idOne)
+
+// class
+let byClass = document.querySelector('.two')
+console.log(byClass)
+
+//tagName
+let byTagName =document.querySelector('h4')
+console.log(byTagName )
---
+++ DOM/DOM4/day.js
@@ -0,0 +1,10 @@
+//<h4>id = "one" class = "two" name = "nm" >prasad4</h4>
+
+
+// id
+let idOne = document.querySelector("#one")
+console.log(idOne)
+
+// class
+
+let by class = 
